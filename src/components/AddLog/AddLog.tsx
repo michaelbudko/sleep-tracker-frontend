0a69d434e3a6ebe33e8a970664541b04b6cc2894
Mikhail Budko <mikhailbudko@mikhails-mbp-2.mynetworksettings.com>
2023-11-15 22:38:44 -0500

    added all inputs

--- src/components/AddLog/AddLog.tsx
+++ src/components/AddLog/AddLog.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { ErrorMessage, useFormik } from 'formik';
 import * as Yup from 'yup';
 import { Button, FormGroup, FormControl, FormLabel, Alert } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 const AddLog = () => {
+
   const validationSchema = Yup.object().shape({
     night: Yup.string()
       .matches(/^(monday|tuesday|wednesday|thursday|friday|saturday|sunday)$/i, 'Invalid Day of the Week')
@@ -12,16 +14,28 @@ const AddLog = () => {
       .required('Date is required'),
   });
 
+  const navigate = useNavigate();
+
   const formik = useFormik({
     initialValues: {
       night: '',
       date: '',
       timeWentToBed: '',
+      timeTookToFallAsleep: '',
+      numTimesWokeUp: '',
+      finalAwakening: '',
+      gotOutOfBed: '',
+      hoursSlept: '',
+      hoursAllotted: '',
+      sleepQuality: '',
+      sleepMeds: '',
+      notes: ''
     },
     validationSchema: validationSchema,
     onSubmit: (values) => {
       // Your form submission logic here
       console.log(values);
+      navigate('/');
     },
   });
 
@@ -59,7 +73,7 @@ const AddLog = () => {
 
         {/* Time went to bed */}
         <FormGroup className='mt-3' controlId='timeWentToBed'>
-            <label className='d-block mb-2'>Last night, what time did you go to bed?</label>
+            <label className='d-block mb-2'>Last night, what time did you go to bed? (lights out)</label>
             <input
             type="text"
             id='timeWentToBed'
@@ -72,8 +86,145 @@ const AddLog = () => {
             {/* <ErrorMessage name="date" render={(msg) => <Alert variant="danger">{msg}</Alert>} /> */}
         </FormGroup>
 
+        {/* Time took to fall asleep */}
+        <FormGroup className='mt-3' controlId='timeTookToFallAsleep'>
+            <label className='d-block mb-2'>About how long did it take you to fall asleep?</label>
+            <input
+            type="text"
+            id='timeTookToFallAsleep'
+            name='timeTookToFallAsleep'
+            value={formik.values.timeTookToFallAsleep}
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+            placeholder="0:25"
+            />
+            {/* <ErrorMessage name="date" render={(msg) => <Alert variant="danger">{msg}</Alert>} /> */}
+        </FormGroup>
+
+        {/* Number of times woke up */}
+        <FormGroup className='mt-3' controlId='numTimesWokeUp'>
+            <label className='d-block mb-2'>About how long did it take you to fall asleep?</label>
+            <input
+            type="number"
+            id='numTimesWokeUp'
+            name='numTimesWokeUp'
+            value={formik.values.numTimesWokeUp}
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+            placeholder="1"
+            />
+            {/* <ErrorMessage name="date" render={(msg) => <Alert variant="danger">{msg}</Alert>} /> */}
+        </FormGroup>
+
+        {/* Time of final awakening */}
+        <FormGroup className='mt-3' controlId='finalAwakening'>
+            <label className='d-block mb-2'>What time was your final awakening this morning?</label>
+            <input
+            type="text"
+            id='finalAwakening'
+            name='finalAwakening'
+            value={formik.values.finalAwakening}
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+            placeholder="8:00am"
+            />
+            {/* <ErrorMessage name="date" render={(msg) => <Alert variant="danger">{msg}</Alert>} /> */}
+        </FormGroup>
+
+        {/* Time got out of bed */}
+        <FormGroup className='mt-3' controlId='gotOutOfBed'>
+            <label className='d-block mb-2'>What time did you get out of bed?</label>
+            <input
+            type="text"
+            id='gotOutOfBed'
+            name='gotOutOfBed'
+            value={formik.values.gotOutOfBed}
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+            placeholder="8:00am"
+            />
+            {/* <ErrorMessage name="date" render={(msg) => <Alert variant="danger">{msg}</Alert>} /> */}
+        </FormGroup>
+
+        {/* How many hours did you sleep */}
+        <FormGroup className='mt-3' controlId='hoursSlept'>
+            <label className='d-block mb-2'>What time did you get out of bed?</label>
+            <input
+            type="text"
+            id='hoursSlept'
+            name='hoursSlept'
+            value={formik.values.hoursSlept}
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+            placeholder="8:00"
+            />
+            {/* <ErrorMessage name="date" render={(msg) => <Alert variant="danger">{msg}</Alert>} /> */}
+        </FormGroup>
+
+        {/* How many hours did you sleep */}
+        <FormGroup className='mt-3' controlId='hoursAllotted'>
+            <label className='d-block mb-2'>How many hours did you allot for sleep last night? (lights out to out of bed)</label>
+            <input
+            type="text"
+            id='hoursAllotted'
+            name='hoursAllotted'
+            value={formik.values.hoursAllotted}
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+            placeholder="8:00"
+            />
+            {/* <ErrorMessage name="date" render={(msg) => <Alert variant="danger">{msg}</Alert>} /> */}
+        </FormGroup>
+        
+        {/* How many hours did you sleep */}
+        <FormGroup className='mt-3' controlId='sleepQuality'>
+            <label className='d-block mb-2'>Rate the quality of last night's sleep (1-5)</label>
+            <input
+            type="number"
+            id='sleepQuality'
+            name='sleepQuality'
+            value={formik.values.sleepQuality}
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+            placeholder="3"
+            />
+            {/* <ErrorMessage name="date" render={(msg) => <Alert variant="danger">{msg}</Alert>} /> */}
+        </FormGroup>
+
+        {/* How many hours did you sleep */}
+        <FormGroup className='mt-3' controlId='sleepMeds'>
+            <label className='d-block mb-2'>Sleep medications taken</label>
+            <input
+            type="text"
+            id='sleepMeds'
+            name='sleepMeds'
+            value={formik.values.sleepMeds}
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+            placeholder="Melatonin: 1mg"
+            />
+            {/* <ErrorMessage name="date" render={(msg) => <Alert variant="danger">{msg}</Alert>} /> */}
+        </FormGroup>
+
+        {/* How many hours did you sleep */}
+        <FormGroup className='mt-3' controlId='notes'>
+            <label className='d-block mb-2'>Additional Notes</label>
+            <input
+            type="text"
+            id='notes'
+            name='notes'
+            value={formik.values.notes}
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+            placeholder="notes"
+            />
+            {/* <ErrorMessage name="date" render={(msg) => <Alert variant="danger">{msg}</Alert>} /> */}
+        </FormGroup>
+
+        
+
         {/* Submit button */}
-        <Button className='mt-3' type='submit'>
+        <Button className='mt-3 mb-3' type='submit'>
             Submit
         </Button>
     </form>
